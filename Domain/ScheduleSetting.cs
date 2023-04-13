48fdef54dfceb8f70bdbf7ec155ffb9fcf0efb5b
LightDDark <myavraham25@gmail.com>
2023-04-13 17:38:48 +0300

    Added Schedule Setting

---
+++ Domain/ScheduleSetting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain {
+    /*"startHour": "9:00:00",
+        "endHour": "18:00:00",
+        "minGap": 15,
+        "maxHoursPerDay": 5,
+        "maxHoursPerTypePerDay": {"A": 3, "B": 2},
+        "minTimeFrame": 15*/
+    public class ScheduleSetting {
+        [Key] int id;
+        DateTime StartHour { get; set; } = new DateTime(0, 0, 0, 9, 0, 0);
+        DateTime EndHour { get; set; } = new DateTime(0, 0, 0, 18, 0, 0);
+        int MinGap { get; set; } = 15;
+        int MaxHoursPerDay { get; set; } = 5;
+        int MinTimeFrame { get; set; } = 15;
+    }
+}
